0238dcac280d4b7467a75ac5c8169c2d3eb45acf
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-07-28 20:42:40 +0000

    refactor functions to retrieve data from our database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -41,6 +41,12 @@ exports.getUserWithEmail = getUserWithEmail;
 const getUserWithId = function(id) {
   return pool
     .query(`SELECT * FROM users WHERE id = $1`, [id])
+    .then((res) => {
+      if (res.rows.length === 0) {
+        return null
+      }
+      return res.rows[0];
+    })
 }
 exports.getUserWithId = getUserWithId;
 
@@ -51,10 +57,11 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return pool
+    .query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *` , [user.name, user.email, user.password])
+    .then(res => {
+      return res.rows;
+    })
 }
 exports.addUser = addUser;
 
